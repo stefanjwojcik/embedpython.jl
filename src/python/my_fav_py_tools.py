e11f39836558f2198e6a0609e4b1e5c79fd70b07
stefanjwojcik <stefan.wojcik@colorado.edu>
2024-01-21 20:14:58 -0600

    test

--- src/python/my_fav_py_tools.py
+++ src/python/my_fav_py_tools.py
@@ -1,8 +1,8 @@
 
 # Create a simple function that prints a statement 
 
-import os
-import sys
+#import os
+#import sys
 #mypath = os.path.join(os.getcwd(), 'src/python')
 #sys.path.append(mypath)
 
